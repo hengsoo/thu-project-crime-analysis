b31aac820a356261ab0b19454177d872c7b7973e
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2019-05-05 18:15:56 +0800

    Done converting raw_crime_data to object

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    // Bootstraps input file
+    $('input[type="file"]').change(function(e){
+        var fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
+    });
+
+});
