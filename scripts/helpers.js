7a7bf2741b04c0a4d674ecf775ccb798b1a9087b
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2019-05-06 16:55:24 +0800

    Add getWeight helper function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,17 +1,27 @@
 // Helper functions
 
-// Declare global help function variables here
+// Declare global helper function variables here
 let convertVertexDatetimeToMoment;
+// Get edges weight
+let getWeight;
 
 // Assign declared function variables to helper functions
-$(document).ready(function(){
+$(document).ready(function () {
 
-    convertVertexDatetimeToMoment = function ( vertex_obj ){
+    convertVertexDatetimeToMoment = function (vertex_obj) {
         for (let index in vertex_obj) {
             // skip loop if the property is from prototype
             if (!vertex_obj.hasOwnProperty(index)) continue;
             vertex_obj[index]["datetime"] = moment(vertex_obj[index]["datetime"], ["MM-DD-YY H:mm", "L LTS"]);
         }
+    };
+
+    getWeight = function (vertex_1, vertex_2) {
+        // swap vertex_1 and vertex_2 to ensure that vertex_1 smaller than vertex_2
+        if (vertex_1 > vertex_2) {
+            [vertex_1, vertex_2] = [vertex_2, vertex_1];
+        }
+        return edge[vertex_1][vertex_2];
     }
 
 });
