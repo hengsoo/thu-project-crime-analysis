6f2b5fc178cce7a7af4d08ba36d70e073b3098ce
ngjuping <ngjuping2018@163.com>
2019-05-10 17:34:13 +0800

    Refactor gitignore Refactor shortest path

--- scripts/shortest_path.js
+++ scripts/shortest_path.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
     
-    //Test case
+    //Driver
     $("#input_start_node, #input_end_node").change(function(e){
         let start = $("#input_start_node").val();
         let end = $("#input_end_node").val();
 
-        console.log(start!=-1 && end!=-1);
+        //if both input area is filled
         if(start!=-1 && end!=-1)
         {
-           shortestPath(start,end);
+            //if data given is between 0 and number of nodes
+            if(start>=0 && end<vertex_data.length)
+            {
+                shortestPath(start,end);
+            }
         }
 
     });
-
-
-    //End of test case
+    //End of driver
     
     //Custom queue function
     function Queue()
     {
-    this.q = [];
-    this.len = 0;
-    this.enqueue = function(val){this.len++; this.q.push(val);}
-    this.dequeue = function(){this.len--; return this.q.shift()}
+        this.q = [];
+        this.len = 0;
+        this.enqueue = function(val)
+        {
+            this.len++; this.q.push(val);
+        }
+        this.dequeue = function()
+        {
+            this.len--; 
+            return this.q.shift();
+        }
     }
 
     //given start and end, finds the shortest path using dijkstra
@@ -31,16 +40,28 @@ $(document).ready(function () {
         const MAX = Infinity;
         const node_count = edge.length+1;
 
+        //double security
+        if(!(start>=0 && end<vertex_data.length))
+        {
+            console.log("start and end node error");
+            //end the function
+            return;
+        }
         //initialize some array that will be used to store parent,shortest path length
-        var shortest = (new Array(node_count)).fill(MAX);
-        var parent = (new Array(node_count)).map((v,index)=>{return index+1});
+        var vertices = [];
+        for(let i=0 ; i<node_count ; i++)
+        {
+            vertices[i] = {};
+            vertices[i].shortest = MAX;
+            vertices[i].parent = 0;
+        }
 
         var q = new Queue;
 
         //initialize the queue with the starting node
         q.enqueue(start);
-        parent[start] = start;
-        shortest[start] = 0;
+        vertices[start].parent = start;
+        vertices[start].shortest = 0;
 
         while(q.len>0) //queue is not empty
         {
@@ -50,31 +71,32 @@ $(document).ready(function () {
             {
                 //for each nodes connected to current node
                 adj_nodes.forEach((next_node)=>{ 
-                //if there is a change in shortest distance from start to next_node
-                if(shortest[current_node]+getWeight(current_node,next_node) < shortest[next_node])
-                {
-                    shortest[next_node] = shortest[current_node]+getWeight(current_node,next_node);
-                    //update parent on the shortest path
-                    parent[next_node] = current_node;
-                    //add it to the queue
-                    q.enqueue(next_node);
-                }
+                    //if there is a change in shortest distance from start to next_node
+                    if(vertices[current_node].shortest + getWeight(current_node,next_node) < vertices[next_node].shortest)
+                    {
+                        vertices[next_node].shortest = vertices[current_node].shortest + getWeight(current_node,next_node);
+                        //update parent on the shortest path
+                        vertices[next_node].parent = current_node;
+                        //add it to the queue
+                        q.enqueue(next_node);
+                    }
                 })
             }
         }
 
-        console.log("Shortest path length: " + shortest[end]) //gives the shortest distance from start to end
+        //gives the shortest distance from start to end
+        console.log("Shortest path length: " + vertices[end].shortest) 
 
         //Computes the path using the parent array
         var steps = [];
-        var paren = parent[end];
+        var parent = vertices[end].parent;
+
         //Because end is not considered in the assignment above, we need to add end explicitly
         steps.unshift(end);
-        while(paren!=start)
+        while(parent!=start)
         {
-            steps.unshift(paren);
-            //console.log(`From node ${paren} to ${parent[paren]} has weight ${getWeight(paren,parent[paren])}`)
-            paren = parent[paren];
+            steps.unshift(parent);
+            parent = vertices[parent].parent;
         }
         //Because start is not considered when the while loop ends, so we need to add start explicitly
         steps.unshift(start);
