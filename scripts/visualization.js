d43d3eabb44991e0f454754ffa604622ae7aac48
hengsoo.sjh@gmail.com <hengsoo.sjh@gmail.com>
2019-05-13 18:06:52 +0800

    Refactor visualization.js and integrate it into index.html

--- scripts/visualization.js
+++ scripts/visualization.js
@@ -1,84 +1,82 @@
 $(document).ready(function () {
 
-    let result = {};
-    result.nodes = [];
-    result.links = [];
+    // Display graph data
+    let graph = {};
+    graph.nodes = [];
+    graph.links = [];
 
+    // Graph nodes
+    // Currently set nodes of community area <= 10
     vertex_data.forEach(function (element, index, array) {
         if (element.community_area < 10) {
-            console.log(element.community_area);
-            result.nodes.push({"id": index, "data": element});
+            graph.nodes.push({"id": index, "data": element});
         }
-
     });
 
-
+    // Graph links
+    // Currently set links that consist of nodes of community area <= 10
     for (let i = 0; i < edge.length; i++) {
         for (let key in edge[i]) {
             if (edge[i][key] !== -1 && vertex_data[key].community_area < 10 && vertex_data[i].community_area < 10) {
-                result.links.push({"source": i, "target": key})
+                graph.links.push({"source": i, "target": key})
             }
         }
     }
 
-    // console.log(JSON.stringify(result));
-
     // Select svg
-    let svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+    let svg = d3.select("#basic_svg"),
+        width = svg.attr("width"),
+        height = svg.attr("height");
 
+    // Create the simulation with nodes data
     let simulation = d3.forceSimulation()
-        .nodes(result.nodes);
+    // Nodes data
+        .nodes(graph.nodes);
 
+    // Add forces
+    // We're going to add a charge (like electrical charge) to each node
+    //  forceManyBody (for making elements attract or repel one another)
+    // Also going to add a centering force (pushing all nodes to center)
     simulation
         .force("charge_force", d3.forceManyBody())
         .force("center_force", d3.forceCenter(width / 2, height / 2));
 
 
-
-    //Create the link force
-    //We need the id accessor to use named sources and targets
-    let link_force = d3.forceLink(result.links)
+    // Create the link force
+    // forceLink(links_data) pushes linked elements to be a fixed distance apart
+    // can be configured using .distance() (default value is 30) and .strength()
+    // We need the id accessor to use named sources and targets
+    let link_force = d3.forceLink(graph.links)
         .id(function (d) {
             return d.id;
         })
         .distance(300);
 
-    simulation.force("links",link_force);
-    //add forces
-    //we're going to add a charge to each node
-    //also going to add a centering force
+    // Add force
+    simulation.force("links", link_force);
 
 
-//draw lines for the links
+    // Draw lines for the links
     let link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
-        .data(result.links)
+        .data(graph.links)
         .enter().append("line")
         .attr("stroke-width", 2);
 
-    //draw circles for the nodes
+    // Draw circles for the nodes
     let node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
-        .data(result.nodes)
+        .data(graph.nodes)
         .enter()
         .append("circle")
         .attr("r", 5)
         .attr("fill", "red");
 
-
+    // Tick can be regarded as how frame moves to frame
     simulation.on("tick", ticked);
-    // d3.select("svg")
-    //     .call(d3.drag()
-    //         .container(svg)
-    //         .subject(dragsubject)
-    //         .on("start", dragstarted)
-    //         .on("drag", dragged)
-    //         .on("end", dragended));
-
+    // Ticked function
     function ticked() {
         node
             .attr("cx", function (d) {
@@ -87,14 +85,16 @@ $(document).ready(function () {
             .attr("cy", function (d) {
                 return d.y;
             })
+            // Dragging properties
             .call(d3.drag()
                 .subject(dragsubject)
                 .on("start", dragstarted)
                 .on("drag", dragged)
-                .on("end", dragended));;
-        // update link positions
-        //simply tells one end of the line to follow one node around
-        //and the other end of the line to follow the other node around
+                .on("end", dragended));
+
+        // Update link positions
+        // Simply tells one end of the line to follow one node around
+        // And the other end of the line to follow the other node around
         link
             .attr("x1", function (d) {
                 return d.source.x;
@@ -110,6 +110,7 @@ $(document).ready(function () {
             });
     }
 
+    // List of drag functions
     function dragsubject() {
         return simulation.find(d3.event.x, d3.event.y);
     }
@@ -130,25 +131,5 @@ $(document).ready(function () {
         d3.event.subject.fx = null;
         d3.event.subject.fy = null;
     }
-
-    function drawLink(d) {
-        context.moveTo(d.source.x, d.source.y);
-        context.lineTo(d.target.x, d.target.y);
-    }
-
-    function drawNode(d) {
-        context.moveTo(d.x + 3, d.y);
-        context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
-
-        if (d.data.community_area == 1) {
-            context.beginPath();
-            context.fillStyle = "#fff";
-
-        } else {
-            context.fillStyle = "#ff7765";
-            context.fill();
-        }
-
-    }
-
+    // End of drag functions
 });
