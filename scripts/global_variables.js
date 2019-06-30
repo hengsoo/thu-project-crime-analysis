c8ceaea4139865078e9763027c457a65a2ccc0e9
ngjuping <ngjuping2018@163.com>
2019-06-30 16:09:04 +0800

    Modify shortest path for self to self Add BC algo Add global var as container for shortest paths between nodes

--- scripts/global_variables.js
+++ scripts/global_variables.js
@@ -5,5 +5,8 @@ let edge_count = 36656;
 // Global vertex_data variables
 let vertex_data = [];
 
+//Shortest path information to be used
+let shortest_paths = [];
+
 // Global closeness centrality variable
 let closeness_centrality = [];
