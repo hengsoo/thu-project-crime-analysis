41a9a9d451904ec6a40084afea498597f4466b33
ngjuping <ngjuping2018@163.com>
2019-06-30 15:56:35 +0800

    Add example for download function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -39,8 +39,13 @@ function getAdjNodes(node_no, adj_mat = edge) {
 }
 
 //Download a json file
-function download(content, fileName, contentType) 
+function download(content, fileName, contentType)
 {
+  //Example of usage:
+  // var obj = {a:514,b:"Hello World"};
+  // download(JSON.stringify(obj),"test.json")
+  // A system alert box will pop up to select download destination
+  // data saved in json file named test
   var a = document.createElement("a");
   var file = new Blob([content], {type: contentType});
   a.href = URL.createObjectURL(file);
